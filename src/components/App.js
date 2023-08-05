b8ff0ad3683688b81350bbb9cf319c1c7f458eb2
saghajani-oss <112084533+saghajani-oss@users.noreply.github.com>
2023-08-05 20:12:03 +0330

    Editor component

---
+++ src/components/App.js
@@ -0,0 +1,55 @@
+import React, { useState, useEffect } from "react";
+import Editor from "./Editor";
+
+function App() {
+const[html, setHtml] = useState('')
+const[css, setCss] = useState('')
+const[js, setJs] = useState('')
+const[srcDoc, setSrcDoc] = useState('')
+
+useEffect(() => {
+  const timeout = setTimeout(() =>{
+    setSrcDoc(
+      `<html>
+        <body>${html}</body>
+        <style>${css}</style>
+        <script>${js}</script>
+      </html>`
+    );
+  }, 250)
+  return () => clearTimeout(timeout) 
+}, [html, css, js])
+
+  return (
+    <div className="App">
+      <div className="pane top-pane">
+        <Editor language="xml"
+        displayName="HTML"
+        value={html}
+        onChange={setHtml} />
+        <Editor 
+        language="css"
+        displayName="CSS"
+        value={css}
+        onChange={setCss} />
+        <Editor
+        language="javascript"
+        displayName="JS"
+        value={js}
+        onChange={setJs} />
+      </div>
+      <div className="pane">
+        <iframe
+        srcDoc={srcDoc}
+          title="output"
+          sandbox="allow-script"
+          width="100%"
+          height="100%"
+          frameBorder="0"
+        ></iframe>
+      </div>
+    </div>
+  );
+}
+
+export default App;
